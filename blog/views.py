cccffc4b1e393ca7e7a9cc22b39bdbee446c6628
homilly <homillyxiao@gmail.com>
2018-12-28 01:16:03 +0800

    https://docs.djangoproject.com/en/2.1/intro/tutorial02/

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse("Hello ,world! I am in bolg view now.")
 # Create your views here.
